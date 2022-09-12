4c44c4f6bacfc198e2b84011fed6b43e7da83fb9
Filip Tangen <filiptangen1998@live.com>
2022-09-12 15:49:35 +0200

    comments

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import { getState } from "./state";
   const state = getState();
 
+  // Increments the value of the count variable in the state.
   function add() {
     $state.count += 1;
   }
@@ -9,6 +10,7 @@
 
 <div class="h-full w-full flex justify-center items-center">
   <button class="btn btn-primary" on:click={add}>
+    <!-- Displays the current value stored in the state -->
     {$state.count}
   </button>
 </div>
--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,8 @@
 import { state, type State } from "poxi";
+// Defines the variables you wait available in the store.
 type Schema = { count: number };
+
+// The state function returns a svelte store that allows you to edit the state
 export const getState = () => state<Schema>();
 
 //Initialize the counter variable with value 0
