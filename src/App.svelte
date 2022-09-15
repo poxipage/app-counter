19833d9f78853692ecaa106eadb20c09030c6100
Filip Tangen <filiptangen1998@live.com>
2022-09-15 12:21:27 +0200

    fix links

--- src/App.svelte
+++ src/App.svelte
@@ -16,9 +16,9 @@
 <style>
   /*Use normal css if you don't want to use Tailwindcss and/or DaisyUI */
   div {
-    @apply h-full w-full flex justify-center items-center; /*Center the elements within the div using Tailwindcss https://tailwindcss.com*/
+    @apply h-full w-full flex justify-center items-center; /*Center the elements within the div using Tailwindcss https://tailwindcss.com */
   }
   button {
-    @apply btn btn-primary; /* From DaisyUI https://daisyui.com*/
+    @apply btn btn-primary; /* From DaisyUI https://daisyui.com */
   }
 </style>
