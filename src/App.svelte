5765fb5c8eb941def107959bf4e988112828aa5a
Filip Tangen <filiptangen1998@live.com>
2022-09-15 12:20:54 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
 <style>
   /*Use normal css if you don't want to use Tailwindcss and/or DaisyUI */
   div {
-    @apply h-full w-full flex justify-center items-center; /*From Tailwindcss https://tailwindcss.com*/
+    @apply h-full w-full flex justify-center items-center; /*Center the elements within the div using Tailwindcss https://tailwindcss.com*/
   }
   button {
     @apply btn btn-primary; /* From DaisyUI https://daisyui.com*/
