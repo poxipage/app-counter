4d47252ed998ec9a5ad0387c429a25747319efa8
Filip Tangen <filiptangen1998@live.com>
2022-09-15 11:15:34 +0200

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,15 @@
 <script lang="ts">
   import { getState } from "./state";
   const state = getState();
-
-  // Increments the value of the count variable in the state.
-  function add() {
-    $state.count += 1;
-  }
 </script>
 
 <div class="h-full w-full flex justify-center items-center">
-  <button class="btn btn-primary" on:click={add}>
-    <!-- Displays the current value stored in the state -->
+  <button
+    class="btn btn-primary"
+    on:click={() => {
+      $state.count += 1;
+    }}
+  >
     {$state.count}
   </button>
 </div>
